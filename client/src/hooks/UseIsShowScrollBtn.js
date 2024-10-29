57c4e8691396f09773badda2c833081f39bb1aa0
Egorlaptop <egorlavrinovich@gmail.com>
2024-10-29 19:03:15 +0300

    #5 update custom hook scroll down

--- client/src/hooks/UseIsShowScrollBtn.js
+++ client/src/hooks/UseIsShowScrollBtn.js
@@ -1,11 +1,11 @@
 import {BORDER_FROM_USER_MESSAGE} from "../constants/chat.js";
-import {useEffect, useState, useRef} from "react";
+import {useLayoutEffect, useRef, useState} from "react";
 import _ from "lodash";
 import {scrollToLastMessage} from "../helpers/chat.js";
 
 export const UseIsShowScrollBtn = (messages) => {
     const ref = useRef()
-    const [isShow, setIsShow] = useState(false)
+    const [isShow, setIsShow] = useState()
 
     const onHandleScroll = _.debounce(() => {
         const currentPositionScroll = ref?.current?.scrollTop
@@ -15,14 +15,14 @@ export const UseIsShowScrollBtn = (messages) => {
         setIsShow(heightAllBlock - heightViewBlock - BORDER_FROM_USER_MESSAGE > currentPositionScroll)
     }, 200, {maxWait: 5000})
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         onHandleScroll()
         ref?.current?.addEventListener('scroll', onHandleScroll)
         return () => ref?.current?.removeEventListener('scroll', onHandleScroll)
     }, [ref])
 
-    useEffect(() => {
-        if (!isShow) scrollToLastMessage()
+    useLayoutEffect(() => {
+        if (Boolean(isShow) && !isShow) scrollToLastMessage()
     }, [isShow, messages?.length])
 
     return [isShow, ref]
